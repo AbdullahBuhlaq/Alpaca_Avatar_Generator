76ecc261cd2c4eaad86bf94e0da125b204364b8f
Abdullah Buhlaq <3bdullah.bu7la8@gmail.com>
2022-09-18 17:13:21 +0300

    make it betterrrrr

--- app.js
+++ app.js
@@ -10,18 +10,22 @@ if (process.env.NODE_ENV === "production") {
   app.get("/", (req, res) => {
     res.sendfile(path, resolve(__dirname, "/build", "index.html"));
   });
-  app.post("/getTupe", async (req, res) => {
-    let result = {};
+  app.post(
+    "https://alpaca-avatar-generator.herokuapp.com/getType",
+    async (req, res) => {
+      // let result = {};
 
-    let x = await fs.readdir(__dirname + "/build/alpaca");
+      // let x = await fs.readdir(__dirname + "/build/alpaca");
 
-    for (let i = 0; i < x.length; i++) {
-      let value = await fs.readdir(__dirname + "/build/alpaca/" + x[i]);
-      result = { ...result, [x[i]]: value };
+      // for (let i = 0; i < x.length; i++) {
+      //   let value = await fs.readdir(__dirname + "/build/alpaca/" + x[i]);
+      //   result = { ...result, [x[i]]: value };
+      // }
+
+      // res.send(result);
+      res.send({ id: 3 });
     }
-
-    res.send(result);
-  });
+  );
 } else {
   app.use(express.static("build"));
   app.get("/", (req, res) => {
