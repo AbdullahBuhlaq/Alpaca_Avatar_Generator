d3ab8f7b8084e6b81eef9245c257ed3b27c71dd0
Abdullah Buhlaq <3bdullah.bu7la8@gmail.com>
2022-09-18 16:55:56 +0300

    make it betterrrrr

--- app.js
+++ app.js
@@ -5,19 +5,35 @@ const { resolve } = require("path");
 const app = express();
 const path = require("path");
 const port = process.env.PORT || 5000;
-console.log("hihihihih");
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
   app.get("/", (req, res) => {
-    res.sendfile(path, resolve(path, "build", "index.html"));
+    res.sendfile(path, resolve(__dirname, "/build", "index.html"));
   });
   app.post("/app", async (req, res) => {
     let result = {};
 
-    let x = await fs.readdir(path + "alpaca");
+    let x = await fs.readdir(__dirname + "/build/alpaca");
 
     for (let i = 0; i < x.length; i++) {
-      let value = await fs.readdir(path + "alpaca/" + x[i]);
+      let value = await fs.readdir(__dirname + "/build/alpaca/" + x[i]);
+      result = { ...result, [x[i]]: value };
+    }
+
+    res.send(result);
+  });
+} else {
+  app.use(express.static("build"));
+  app.get("/", (req, res) => {
+    res.sendfile(path, resolve(__dirname, "/build", "index.html"));
+  });
+  app.post("/getType", async (req, res) => {
+    let result = {};
+
+    let x = await fs.readdir(__dirname + "/build/alpaca");
+
+    for (let i = 0; i < x.length; i++) {
+      let value = await fs.readdir(__dirname + "/build/alpaca/" + x[i]);
       result = { ...result, [x[i]]: value };
     }
 
--- src/components/ChoicesSection.jsx
+++ src/components/ChoicesSection.jsx
@@ -10,15 +10,18 @@ import ImageSection from './ImageSection';
 function ChoicesSection()
 {
 
-
     function init(){
 
-        let data={
-            accessories:["earings.png","flower.png"],
-            backgrounds:["blue50.png","blue60.png"]
-        }
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ title: 'React POST Request Example' })
+        };
 
-        setAllChoices(data)
+        fetch('/getType',requestOptions)
+          .then(response => response.json())
+          .then((data) => {
+              setAllChoices(data)
               let tempTypes=Object.keys(data)
               setTypes(tempTypes)
               setCurrentType(tempTypes[0])
@@ -32,58 +35,20 @@ function ChoicesSection()
               if(item===currentType) setCurrentValue(cand.substring(0,cand.lastIndexOf('.')))
               setImage(tempImage)
               })
-
-        // const requestOptions = {
-        //     method: 'POST',
-        //     headers: { 'Content-Type': 'application/json' },
-        //     body: JSON.stringify({ title: 'React POST Request Example' })
-        // };
-
-        // try{
-
-        // fetch('/app',requestOptions)
-        //   .then((response) => {
-        //     console.log("inside then1")
-        //     response.json()})
-        //   .then((data) => {
-        //     console.log(data)
-        //       setAllChoices(data)
-        //       let tempTypes=Object.keys(data)
-        //       setTypes(tempTypes)
-        //       setCurrentType(tempTypes[0])
-        //       setValues(data[tempTypes[0]])
-            
-        //       let tempImage={}
-        //       tempTypes.forEach((item)=>{
-        //       const rand=Math.floor(Math.random()*data[item].length)
-        //       const cand=data[item][rand]
-        //       tempImage[item]="alpaca/"+item+"/"+cand
-        //       if(item===currentType) setCurrentValue(cand.substring(0,cand.lastIndexOf('.')))
-        //       setImage(tempImage)
-        //       })
         
 
-        //   })
-        // }catch(error){
-        //     console.log("error")
-        //     console.log(error)
-        // }
-
-        
+          })
     }
 
-    const [allChoices,setAllChoices] = useState({
-        accessories:["earings.png","flower.png"],
-        backgrounds:["blue50.png","blue60.png"]
-    }),
-          [types,setTypes] = useState(["accessories","backgrounds"]),
-          [values,setValues] = useState(["earings.png","flower.png"]),
-          [image,setImage] = useState({accessories:"earings.png"}),
-          [currentType,setCurrentType]=useState("accessories"),
-          [currentValue,setCurrentValue]=useState("earings")
+    const [allChoices,setAllChoices] = useState(()=>{init()},{}),
+          [types,setTypes] = useState([]),
+          [values,setValues] = useState([]),
+          [image,setImage] = useState({}),
+          [currentType,setCurrentType]=useState(""),
+          [currentValue,setCurrentValue]=useState("")
 
     useEffect(()=>{
-         if(image && Object.keys(image) && Object.keys(image).length) mergeImages([image.backgrounds,image.accessories])
+         if(image && Object.keys(image) && Object.keys(image).length) mergeImages([image.backgrounds,image.neck,image.hair,image.ears,image.eyes,image.leg,image.mouth,image.nose,image.accessories])
         .then((b64) => {
             document.getElementById('image').src = b64
             document.getElementById('link').href = b64
